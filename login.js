aa8297df847cc2cd9a1d546a43cb15ff64917716
rabkrish <78112475+rabkrish@users.noreply.github.com>
2021-01-28 17:39:58 +0530

    Update login.js

--- login.js
+++ login.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
+	response.sendFile(path.join(__dirname + '/login.htm'));
 });
 
 app.post('/auth', function(request, response) {
